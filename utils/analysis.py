fa52c47784a055132660467ea3fa4d62d38f2c12
Yashika Desai <yashikadesai@Yashikas-MacBook-Pro-2.local>
2025-06-26 13:46:02 -0400

    task and time

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,5 +1,26 @@
 from textblob import TextBlob
 import numpy as np
 
-blob = TextBlob("This is a great project!")
-print(blob.sentiment)
+def analyze_sentiment(message):
+    sentiments = [TextBlob(msg).sentiment.polarity for msg in messages]
+    avg_sentiment = np.mean(sentiments)
+
+    if avg_sentiment<-0.2:
+        return " Ngeative"
+    elif avg_sentiment<0.2:
+        return "Neutral"
+    else:
+        return "positive"
+    
+def analyze_velocity(time_logs):
+    if len(time_logs<3):
+        return "Low"
+    return "Normal"
+
+def analyze_completion(task_progress):
+    avg=sum(t['percent complete'] for t in task_progress)/len(task_progress)
+    if avg<50:
+        return "Behind"
+    return "On track"
+
+    
